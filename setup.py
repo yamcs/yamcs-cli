8cd3da90125d65670ffca1a3a0b1a56e5f9f76e8
Fabian Diet <fabian.diet@spaceapplications.com>
2020-03-13 18:10:10 +0100

    yamcs-cli v1.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ packages = [
 
 setuptools.setup(
     name='yamcs-cli',
-    version='1.1.9',
+    version='1.2.0',
     description='Yamcs Command-Line Tools',
     long_description=readme,
     long_description_content_type='text/markdown',
