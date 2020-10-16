9665ebfb94fea2a80ea2d1106e93fd0b406760ce
Fabian Diet <fabian.diet@spaceapplications.com>
2020-10-16 13:49:29 +0200

    yamcs-cli v1.3.0

--- setup.py
+++ setup.py
@@ -2,51 +2,48 @@ import io
 
 import setuptools
 
-with io.open('README.md', encoding='utf-8') as f:
+with io.open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
 packages = [
-    package for package in setuptools.find_packages()
-    if package.startswith('yamcs')]
+    package for package in setuptools.find_packages() if package.startswith("yamcs")
+]
 
 setuptools.setup(
-    name='yamcs-cli',
-    version='1.2.2',
-    description='Yamcs Command-Line Tools',
+    name="yamcs-cli",
+    version="1.3.0",
+    description="Yamcs Command-Line Tools",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    url='https://github.com/yamcs/yamcs-cli',
-    author='Space Applications Services',
-    author_email='yamcs@spaceapplications.com',
-    license='LGPL',
+    long_description_content_type="text/markdown",
+    url="https://github.com/yamcs/yamcs-cli",
+    author="Space Applications Services",
+    author_email="yamcs@spaceapplications.com",
+    license="LGPL",
     packages=packages,
-    namespace_packages=['yamcs', 'yamcs.cli'],
-    entry_points={
-        'console_scripts': [
-            'yamcs = yamcs.cli.__main__:main'
-        ]
-    },
-    python_requires='>=3.5',
+    namespace_packages=["yamcs", "yamcs.cli"],
+    entry_points={"console_scripts": ["yamcs = yamcs.cli.__main__:main"]},
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Operating System :: OS Independent',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
     ],
-    platforms='Posix; MacOS X; Windows',
+    platforms="Posix; MacOS X; Windows",
     install_requires=[
-        'yamcs-client',
+        "yamcs-client>=1.6.0",
     ],
     extras_require={
-        'kerberos': ['yamcs-client-kerberos'],
+        "kerberos": ["yamcs-client-kerberos"],
     },
     include_package_data=True,
     zip_safe=False,
