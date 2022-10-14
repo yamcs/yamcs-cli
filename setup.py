767b6767c1b2b4cf92643573078a2f928c983b9a
Fabian Diet <fabian.diet@spaceapplications.com>
2022-10-14 23:52:54 +0200

    v1.4.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open("README.md", encoding="utf-8") as f:
 
 setuptools.setup(
     name="yamcs-cli",
-    version="1.4.2",
+    version="1.4.3",
     description="Yamcs Command-Line Tools",
     long_description=readme,
     long_description_content_type="text/markdown",
