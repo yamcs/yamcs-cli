301a018bc1d776cf4e994e9f7c70f749f2cbf562
Fabian Diet <fabian.diet@spaceapplications.com>
2024-03-25 17:20:59 +0100

    v1.4.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open("README.md", encoding="utf-8") as f:
 
 setuptools.setup(
     name="yamcs-cli",
-    version="1.4.8",
+    version="1.4.9",
     description="Yamcs Command-Line Tools",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -36,7 +36,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     platforms="Posix; MacOS X; Windows",
-    install_requires=["argcomplete", "python-dateutil", "yamcs-client>=1.9.4"],
+    install_requires=["argcomplete", "python-dateutil", "yamcs-client>=1.9.5"],
     extras_require={"kerberos": ["yamcs-client-kerberos>=1.2.2"]},
     include_package_data=True,
     zip_safe=False,
